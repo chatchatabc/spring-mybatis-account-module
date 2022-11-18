bf4ea0cca4b48a9fd691992027ece368b9ddc585
pixelDigitTony <tonyboi122450@gmail.com>
2022-11-18 09:31:02 +0800

    chore[DDD] organised file structure

--- src/main/java/com/example/springmybatisaccountmodule/user/application/securities/SecSecurityConfig.java
+++ src/main/java/com/example/springmybatisaccountmodule/user/application/securities/SecSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.springmybatisaccountmodule.user.Infrastructure.securities;
+package com.example.springmybatisaccountmodule.user.infrastructure.securities;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
